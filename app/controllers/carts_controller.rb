ce0e2a27de55ad6ca63e81150f5c147241a27b51
Jack <jackexyou@gmail.com>
2017-10-29 23:23:28 -0400

    Done.

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,3 +1,18 @@
 class CartsController < ApplicationController
+	before_action :load_cart, only: [:show, :checkout]
+  
+  def show
+  end
 
+  def checkout
+    @cart.checkout
+    redirect_to cart_path(@cart)
+    current_user.remove_current_cart
+  end
+
+  private
+
+  def load_cart
+    @cart = Cart.find(params[:id])
+  end
 end
--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,3 +1,21 @@
 class LineItemsController < ApplicationController
+	def create
+    current_user.create_current_cart if !user_has_current_cart?
+    line_item = load_users_cart.add_item(params[:item_id])
+    if line_item.save
+      redirect_to cart_path(load_users_cart)
+    else
+      redirect_to store_path
+    end
+  end
 
+  private
+
+  def user_has_current_cart?
+    !!current_user.current_cart
+  end
+
+  def load_users_cart
+    current_user.current_cart
+  end
 end
